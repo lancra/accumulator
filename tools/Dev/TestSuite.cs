27c435ec274f48aa1505c310f8a327ab5ce55f12
Lance Craig <lancra@outlook.com>
2024-07-13 06:47:08 -0400

    Add test target

---
+++ tools/Dev/TestSuite.cs
@@ -0,0 +1,6 @@
+namespace Accumulator.Dev;
+
+internal sealed record TestSuite(string Name, string Description, IReadOnlyCollection<TestProject> Projects)
+{
+    public override string ToString() => Name;
+}
